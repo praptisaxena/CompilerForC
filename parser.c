70e4d986dd67e25c4f6f4746cb6243d81624bfdd
Aditi Agarwal <130185768+Agarwal04@users.noreply.github.com>
2025-05-18 22:34:37 +0530

    Update parser.c

--- parser.c
+++ parser.c
@@ -271,21 +271,3 @@ void printAST(ASTNode *node, int indent) {
     }
     printAST(node->next, indent);
 }
-
-// Entry point
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <sourcefile>\n", argv[0]);
-        return 1;
-    }
-
-    runLexer(argv[1]);  // Tokenize source file
-
-    currentTokenIndex = 0;
-    ASTNode *ast = parseFunction();
-
-    printf("\n=== Parsed AST ===\n");
-    printAST(ast, 0);
-
-    return 0;
-}
