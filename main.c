f51eb750740af442d4631fc0355dee78dbf8b4c4
Aditi Agarwal <130185768+Agarwal04@users.noreply.github.com>
2025-05-18 22:35:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
-// main.c
 #include <stdio.h>
 #include "lexer.h"
+#include "parser.h"
 
+//main.c
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <source-file.c>\n", argv[0]);
+    if (argc < 2) {
+        printf("Usage: %s <sourcefile>\n", argv[0]);
         return 1;
     }
 
-    runLexer(argv[1]);
+    runLexer(argv[1]);  // Tokenize source file
+
+    currentTokenIndex = 0;
+    ASTNode *ast = parseFunction();
+
+    printf("\n=== Parsed AST ===\n");
+    printAST(ast, 0);
+
     return 0;
 }
