d2099ff43d49fb926d2dbd468dc58b2b8f693ad3
Aditi Agarwal <130185768+Agarwal04@users.noreply.github.com>
2025-05-18 22:32:36 +0530

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,32 @@
+#ifndef AST_H
+#define AST_H
+
+#define MAX_NAME_LEN 100
+
+typedef enum {
+    AST_FUNCTION,
+    AST_BLOCK,
+    AST_IF,
+    AST_WHILE,
+    AST_FOR,
+    AST_EXPRESSION,
+    AST_STATEMENT,
+
+} ASTNodeType;
+
+typedef struct ASTNode {
+    ASTNodeType type;
+
+    char name[MAX_NAME_LEN];
+
+    struct ASTNode *body;
+    struct ASTNode *elseBody;
+
+    struct ASTNode *condition;
+    struct ASTNode *next;  // for linked lists of statements or expressions
+
+} ASTNode;
+
+ASTNode* createNode(ASTNodeType type);
+
+#endif // AST_H
