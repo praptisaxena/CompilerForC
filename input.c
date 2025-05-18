10a004f1ccf1bc9219898fbeaab0b8d39a6fb5a4
Aditi Agarwal <130185768+Agarwal04@users.noreply.github.com>
2025-05-18 23:04:32 +0530

    Create input.c

---
+++ input.c
@@ -0,0 +1,15 @@
+int main() {
+    int x = 5;
+    int y;
+    y = x + 3;
+    
+    if (y > 5) {
+        y = y - 1;
+    } else {
+        y = y + 1;
+    }
+    
+    while (y < 10) {
+        y = y + 1;
+    }
+}
