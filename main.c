70662711c7732f0dc036ceb354a4ad1fcbb6be22
Aditi Agarwal <130185768+Agarwal04@users.noreply.github.com>
2025-05-26 17:59:56 +0530

    Updated main.c

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "lexer.h"
 #include "parser.h"
+#include "codegen.h"
+
+void analyzeAST(ASTNode *node);
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -11,10 +14,24 @@ int main(int argc, char *argv[]) {
     runLexer(argv[1]);  // Tokenize source file
 
     currentTokenIndex = 0;
-    ASTNode *ast = parseFunction();
+    ASTNode *ast = parseProgram();
 
     printf("\n=== Parsed AST ===\n");
     printAST(ast, 0);
+
+    analyzeAST(ast);         // Run semantic checks
+    printf("Semantic analysis successful.\n");
+
+    // Generate TAC from AST
+    generateCode(ast);
+    printIntermediateCode("Initial");
+
+    // Optimize TAC
+    optimize();
+    printIntermediateCode("Optimized");
+
+    // Generate final assembly
+    generateFinalCode();
 
     return 0;
 }
